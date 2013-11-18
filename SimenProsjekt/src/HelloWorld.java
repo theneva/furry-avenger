7ff704a8e4de629da2ad35cabc83c3dad16ca1ea
Simen Bekkhus <Simen.Bekkhus@accenture.com>
2013-11-18 16:54:04 +0100

    Add HelloWorld.java

---
+++ SimenProsjekt/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Martin
+ * Date: 11/18/13
+ * Time: 4:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
